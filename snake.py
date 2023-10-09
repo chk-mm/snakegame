399270eb97f6431c3e3e1df3af7d855c4a97f25c
Chonnakarn Rungjaroenporn <chonnak.rung@gmail.com>
2023-10-09 09:50:39 +0700

    add a snake at the center of position, hard code the position to be order

--- snake.py
+++ snake.py
@@ -1,10 +1,17 @@
-from turtle import Screen
-
-
+from turtle import Screen, Turtle
 
 screen = Screen()
 screen.bgcolor('black')
 screen.setup(width=600,height=600)
+segments = []
+positions = [(0,0),(0,20),(0,40)]
+for pos in positions:
+    new_turtle = Turtle()
+    new_turtle.shape('square')
+    new_turtle.color('white')
+    new_turtle.goto(pos)
+    segments.append(new_turtle)
+
 
 screen.exitonclick()
 
