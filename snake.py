f8a010b2cdb2886dd2415961df6ed6a9ed704389
Chonnakarn Rungjaroenporn <chonnak.rung@gmail.com>
2023-10-09 10:41:38 +0700

    add tracer in order to turn off the animation, and .update to make it move one more time

--- snake.py
+++ snake.py
@@ -7,6 +7,7 @@ screen = Screen()
 screen.bgcolor('black')
 screen.setup(width=600,height=600)
 segments = []
+screen.tracer(0)
 positions = [(0,0),(20,0),(40,0)]
 for pos in positions:
     new_turtle = Turtle()
@@ -21,6 +22,7 @@ for pos in positions:
 game_is_on = True
 while game_is_on:
     time.sleep(1)
+    screen.update()
     for i in segments:
         i.speed(1)
         i.forward(step)
