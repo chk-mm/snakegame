00bdbcd168b7ba5e1eae1e640f41a1234ef559b7
Chonnakarn Rungjaroenporn <chonnak.rung@gmail.com>
2023-10-09 23:58:09 +0700

    input fruit

--- fruits.py
+++ fruits.py
@@ -6,14 +6,17 @@ from turtle import Turtle
 
 class Fruits:
     food = Turtle()
-    def __init__(self):
+    def __init__(self,filename):
         self.food.speed(0)
-        self.food.shape("circle")
-        self.food.color("red")
+        if filename:
+            self.food.shape(filename)
+        else:
+            self.food.shape("circle")
+            self.food.color("red")
         self.food.penup()
         self.food.goto(0, random.randint(-280,280))
         self.fruit_pos = self.food.position()
 
     def re_position(self):
-        self.food.goto(0, random.randint(-280, 280))
+        self.food.goto(random.randint(-280, 280), random.randint(-280, 280))
         self.fruit_pos = self.food.position()
