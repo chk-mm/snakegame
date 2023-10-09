eb3cdb30d8ef56b542471bb05d472f5e277d4fbc
Chonnakarn Rungjaroenporn <chonnak.rung@gmail.com>
2023-10-09 10:10:29 +0700

    I make the snake move!!!

--- snake.py
+++ snake.py
@@ -1,16 +1,25 @@
 from turtle import Screen, Turtle
 
+step = 20
+
 screen = Screen()
 screen.bgcolor('black')
 screen.setup(width=600,height=600)
 segments = []
-positions = [(0,0),(0,20),(0,40)]
+positions = [(0,0),(20,0),(40,0)]
 for pos in positions:
     new_turtle = Turtle()
     new_turtle.shape('square')
     new_turtle.color('white')
+    new_turtle.pu()
     new_turtle.goto(pos)
     segments.append(new_turtle)
+
+while True:
+    for i in segments:
+        # i.pu()
+        i.forward(step)
+    step += 20
 
 
 screen.exitonclick()
