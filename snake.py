57514552aa679183df45c809f93545aeff1c95ef
Chonnakarn Rungjaroenporn <chonnak.rung@gmail.com>
2023-10-10 10:43:53 +0700

    new food class

--- snake.py
+++ snake.py
@@ -1,6 +1,6 @@
-import random
 from turtle import Turtle
-import fruits
+from score import Score
+from Food import Food
 
 positions = [(0, 0), (20, 0), (40, 0)]
 BOTTOM = 270
@@ -21,23 +21,6 @@ class Snake:
             self.segments.append(new_turtle)
         self.head = self.segments[-1]
         self.is_dead = 0
-        self.score = 0
-        self.score_display = Turtle()
-        self.score_display.speed(0)
-        self.score_display.color("white")
-        self.score_display.penup()
-        self.score_display.hideturtle()
-        self.score_display.goto(0, 260)
-        self.score_display.write("Score: 0", align="center", font=("Courier", 24, "normal"))
-
-    def write_score(self,point):
-        if point > 0:
-            self.score += 10
-            self.score_display.clear()
-            self.score_display.write("Score: {}".format(self.score), align="center", font=("Courier", 24, "normal"))
-        else:
-            self.score_display.clear()
-            self.score_display.write("GAME OVER!!", align="center", font=("Courier", 24, "normal"))
 
     def move(self):
         for i in range(0,len(self.segments)-1):
@@ -63,32 +46,33 @@ class Snake:
         if self.head.heading() != RIGHT:
             self.head.setheading(LEFT)
 
-    def distance_food(self,food: fruits):
-        if self.head.distance(food.fruit_pos) < 20:
+    def distance_food(self,food: Food,scores: Score):
+        if self.head.distance(food.pos()) < 20:
             new_turtle = Turtle()
             new_turtle.shape('square')
-            color = random.choice(['red','blue','purple','yellow'])
+            # color = random.choice(['red','blue','purple','yellow'])
+            color = 'white'
             new_turtle.color(color)
             new_turtle.pu()
             new_turtle.goto(self.segments[0].pos())
             self.segments.insert(0,new_turtle)
             self.head = self.segments[-1]
-            food.re_position()
-            self.write_score(10)
+            food.food_pos()
+            scores.write_score(10)
 
-    def distance_self(self):
+    def distance_self(self,scores: Score):
         for segment in self.segments:
             if self.head.pos() == segment.pos() and segment != self.head:
                 self.is_dead = 1
                 print(self.is_dead)
-                self.write_score(-1)
+                scores.write_score(-1)
 
-    def distance_wall(self):
+    def distance_wall(self,scores: Score):
         if (self.head.xcor() > 290 or
         self.head.xcor() < -290 or
         self.head.ycor() > 290 or
         self.head.ycor() < -290):
             self.is_dead = 1
             print(self.is_dead)
-            self.write_score(-1)
+            scores.write_score(-1)
 
